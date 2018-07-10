48bdb17d0004c075312362609ec72aa4bc805163
ThatGuy1597 <36169733+ThatGuy1597@users.noreply.github.com>
2018-07-10 09:46:11 +0530

    lalalalla

--- rentnow.php
+++ rentnow.php
@@ -3,12 +3,12 @@
     include_once 'database.php';
 
     $u_id = $_SESSION['user_id'];
-    $book_id = $_GET['book_id'];
+    $book_id = $_GET['book_id'];   
+    $scheme_id = $_POST['offers'];
 
-   
-    $query = " INSERT INTO tbl_cart (book_id, u_id) VALUES ('$book_id' , '$u_id') ";
+    
+    $query = " INSERT INTO tbl_cart (book_id, u_id, scheme_id) VALUES ('$book_id' , '$u_id' , '$scheme_id') ";
     $run = mysqli_query($conn, $query);
     
      header('Location: ' .$_SERVER['HTTP_REFERER'] );
 ?>
-
