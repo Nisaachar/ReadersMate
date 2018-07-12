d1c99b945467562e90902fc34ebc65d1ee8f8930
bhunesh2k14 <bhunesh2k14@live.com>
2018-07-12 10:14:18 +0530

    Revert "rewverting"
    This reverts commit 8c1762c510b5ce1902643cd13dfe5c489a37a257.

--- Admin/footer.php
+++ Admin/footer.php
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
   <?php
 
       
-=======
-  <?php 
-  }
-  else {
-      echo "<script>window.open('login.php','_self')</script>";
-  }
->>>>>>> parent of c248050... adding The price tab in tables
   ?>
